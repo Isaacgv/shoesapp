2ae0ba79bf2a277506987f7f22917b429bb0717b
Isaac <isaacgonzalesv@gmail.com>
2025-01-27 23:04:05 +0100

    add identification click button in the notification

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
+import { useEffect } from 'react';
 import { StatusBar, Platform } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { OneSignal } from "react-native-onesignal"
+import { NotificationClickEvent, OneSignal } from "react-native-onesignal"
 import { Routes } from './src/routes';
 
 import { tagUserEmailCreate } from "./src/notifications/notificationsTags"
@@ -20,7 +21,33 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserEmailCreate("EMAIL")
-  
+
+  useEffect(() => {
+    const handleNotificationClick = (event: NotificationClickEvent): void =>{
+      const { actionId } = event.result
+
+      switch(actionId) {
+        case "1":
+          console.log("View all")
+          break
+        case "2":
+          console.log("View commande")
+          break
+        default: 
+          console.log("Not action button selected")
+          break
+
+      }
+    }
+
+    OneSignal.Notifications.addEventListener("click", handleNotificationClick)
+
+    return () => OneSignal.Notifications.removeEventListener(
+      "click", 
+      handleNotificationClick)
+
+  }, [])
+
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
