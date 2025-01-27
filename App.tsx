68c8a92c51595fb8bd2fdc03eba00288040a736a
Isaac <isaacgonzalesv@gmail.com>
2025-01-27 12:37:03 +0100

    add onesignal

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import { StatusBar } from 'react-native';
+import { StatusBar, Platform } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-
+import { OneSignal } from "react-native-onesignal"
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+const oneSignalAppId = Platform.OS == "ios" ? "IOS_API_ID_ONESIGNAL" : "ANDROID_API_ID_ONESIGNAL"
+
+OneSignal.initialize(oneSignalAppId)
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 
 # Install OneSignal
 npx expo install onesignal-expo-plugin@2.0.2
+
+npm i react-native-onesignal@5.0.5
+
+
+# Expo Prebuild
+npx expo prebuild
+
+# Run build Android
+npx expo run:android
+# Run with expo
+npx expo install expo-dev-client
